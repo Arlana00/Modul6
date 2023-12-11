f333fd135a59b445276657e40bc8443b75b77efe
Arlana00 <72141320+Arlana00@users.noreply.github.com>
2023-12-11 07:57:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import time
+from unittest import result
 from flask import Flask, render_template, request
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 app = Flask(__name__)
@@ -13,21 +15,20 @@ model = load_model('model.h5')
 model.make_predict_function()
 
 def predict_label(img_path):
+	start_time = time.time()
 	i = load_img(img_path, target_size=(224,224))
 	i = img_to_array(i)/255.0
 	i = i.reshape(1, 224,224,3)
 	p = model.predict(i)
+	finish_time = time.time()
+	prediction_time = finish_time - start_time
 	index = np.argmax(p, axis=-1)
-	return dic[index[0]]
+	return dic[index[0]], prediction_time
 
 # routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
 	return render_template("index.html")
-
-@app.route("/about")
-def about_page():
-	return " "
 
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
@@ -37,9 +38,10 @@ def get_output():
 		img_path = "static/" + img.filename	
 		img.save(img_path)
 
-		p = predict_label(img_path)
-
-	return render_template("index.html", prediction = p, img_path = img_path)
+		p, prediction_time  = predict_label(img_path)
+		
+		
+	return render_template("index.html", prediction = p,prediction_time=prediction_time,img_path = img_path)
 
 
 if __name__ =='__main__':
